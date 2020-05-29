feaba8d4f2b21a19db4e10823de0969eb15e2007
mullermeck <45013900+mullermeck@users.noreply.github.com>
2020-05-29 02:49:40 +0200

    Färdigt

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -43,8 +43,16 @@
             this.Cone2 = new System.Windows.Forms.PictureBox();
             this.Cone3 = new System.Windows.Forms.PictureBox();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
-            this.timer2 = new System.Windows.Forms.Timer(this.components);
             this.Distance = new System.Windows.Forms.Label();
+            this.Cone4 = new System.Windows.Forms.PictureBox();
+            this.Cone6 = new System.Windows.Forms.PictureBox();
+            this.Cone5 = new System.Windows.Forms.PictureBox();
+            this.Cone7 = new System.Windows.Forms.PictureBox();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.Cone0 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.Cone1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
@@ -57,14 +65,19 @@
             ((System.ComponentModel.ISupportInitialize)(this.Car)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.Cone2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.Cone3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Cone4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Cone6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Cone5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Cone7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Cone0)).BeginInit();
             this.SuspendLayout();
             // 
             // Cone1
             // 
-            this.Cone1.Image = global::_2D_Subway_Surfers_meet_Mario_Kart.Properties.Resources.cone1;
-            this.Cone1.Location = new System.Drawing.Point(56, 46);
+            this.Cone1.BackColor = System.Drawing.Color.Red;
+            this.Cone1.Location = new System.Drawing.Point(110, 101);
             this.Cone1.Name = "Cone1";
-            this.Cone1.Size = new System.Drawing.Size(40, 50);
+            this.Cone1.Size = new System.Drawing.Size(40, 80);
             this.Cone1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.Cone1.TabIndex = 9;
             this.Cone1.TabStop = false;
@@ -143,32 +156,31 @@
             // 
             // Car
             // 
-            this.Car.BackColor = System.Drawing.Color.Transparent;
+            this.Car.BackColor = System.Drawing.Color.Blue;
             this.Car.Cursor = System.Windows.Forms.Cursors.Default;
-            this.Car.Image = ((System.Drawing.Image)(resources.GetObject("Car.Image")));
-            this.Car.Location = new System.Drawing.Point(140, 460);
+            this.Car.Location = new System.Drawing.Point(165, 460);
             this.Car.Name = "Car";
-            this.Car.Size = new System.Drawing.Size(90, 90);
+            this.Car.Size = new System.Drawing.Size(40, 50);
             this.Car.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.Car.TabIndex = 8;
             this.Car.TabStop = false;
             // 
             // Cone2
             // 
-            this.Cone2.Image = global::_2D_Subway_Surfers_meet_Mario_Kart.Properties.Resources.cone1;
-            this.Cone2.Location = new System.Drawing.Point(166, 103);
+            this.Cone2.BackColor = System.Drawing.Color.Red;
+            this.Cone2.Location = new System.Drawing.Point(275, 25);
             this.Cone2.Name = "Cone2";
-            this.Cone2.Size = new System.Drawing.Size(40, 50);
+            this.Cone2.Size = new System.Drawing.Size(40, 80);
             this.Cone2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.Cone2.TabIndex = 10;
             this.Cone2.TabStop = false;
             // 
             // Cone3
             // 
-            this.Cone3.Image = global::_2D_Subway_Surfers_meet_Mario_Kart.Properties.Resources.cone1;
-            this.Cone3.Location = new System.Drawing.Point(276, 255);
+            this.Cone3.BackColor = System.Drawing.Color.Red;
+            this.Cone3.Location = new System.Drawing.Point(275, 225);
             this.Cone3.Name = "Cone3";
-            this.Cone3.Size = new System.Drawing.Size(40, 50);
+            this.Cone3.Size = new System.Drawing.Size(40, 80);
             this.Cone3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.Cone3.TabIndex = 11;
             this.Cone3.TabStop = false;
@@ -176,19 +188,104 @@
             // timer1
             // 
             this.timer1.Enabled = true;
-            this.timer1.Interval = 1;
+            this.timer1.Interval = 15;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // Distance
             // 
-            this.Distance.AutoSize = true;
             this.Distance.BackColor = System.Drawing.Color.Transparent;
             this.Distance.Font = new System.Drawing.Font("Comic Sans MS", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Distance.Location = new System.Drawing.Point(133, 0);
+            this.Distance.Location = new System.Drawing.Point(100, 0);
             this.Distance.Name = "Distance";
-            this.Distance.Size = new System.Drawing.Size(110, 38);
+            this.Distance.Size = new System.Drawing.Size(170, 38);
             this.Distance.TabIndex = 12;
-            this.Distance.Text = "0000 m";
+            this.Distance.Text = "0000";
+            this.Distance.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Cone4
+            // 
+            this.Cone4.BackColor = System.Drawing.Color.Red;
+            this.Cone4.Location = new System.Drawing.Point(55, 290);
+            this.Cone4.Name = "Cone4";
+            this.Cone4.Size = new System.Drawing.Size(40, 80);
+            this.Cone4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Cone4.TabIndex = 13;
+            this.Cone4.TabStop = false;
+            // 
+            // Cone6
+            // 
+            this.Cone6.BackColor = System.Drawing.Color.Red;
+            this.Cone6.Location = new System.Drawing.Point(55, 512);
+            this.Cone6.Name = "Cone6";
+            this.Cone6.Size = new System.Drawing.Size(40, 80);
+            this.Cone6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Cone6.TabIndex = 14;
+            this.Cone6.TabStop = false;
+            // 
+            // Cone5
+            // 
+            this.Cone5.BackColor = System.Drawing.Color.Red;
+            this.Cone5.Location = new System.Drawing.Point(275, 424);
+            this.Cone5.Name = "Cone5";
+            this.Cone5.Size = new System.Drawing.Size(40, 80);
+            this.Cone5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Cone5.TabIndex = 15;
+            this.Cone5.TabStop = false;
+            // 
+            // Cone7
+            // 
+            this.Cone7.BackColor = System.Drawing.Color.Red;
+            this.Cone7.Location = new System.Drawing.Point(110, 356);
+            this.Cone7.Name = "Cone7";
+            this.Cone7.Size = new System.Drawing.Size(40, 80);
+            this.Cone7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Cone7.TabIndex = 16;
+            this.Cone7.TabStop = false;
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 50;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Cone0
+            // 
+            this.Cone0.BackColor = System.Drawing.Color.Red;
+            this.Cone0.Location = new System.Drawing.Point(220, 162);
+            this.Cone0.Name = "Cone0";
+            this.Cone0.Size = new System.Drawing.Size(40, 80);
+            this.Cone0.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Cone0.TabIndex = 17;
+            this.Cone0.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(105, 40);
+            this.panel1.TabIndex = 18;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Orange;
+            this.panel2.Location = new System.Drawing.Point(45, 84);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(280, 343);
+            this.panel2.TabIndex = 19;
+            this.panel2.Visible = false;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(259, -1);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 41);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "Press Enter to \r\nRestart";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // Form1
             // 
@@ -196,7 +293,15 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Gray;
             this.ClientSize = new System.Drawing.Size(369, 561);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel2);
             this.Controls.Add(this.Distance);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.Cone0);
+            this.Controls.Add(this.Cone7);
+            this.Controls.Add(this.Cone5);
+            this.Controls.Add(this.Cone6);
+            this.Controls.Add(this.Cone4);
             this.Controls.Add(this.pictureBox6);
             this.Controls.Add(this.Cone3);
             this.Controls.Add(this.Cone2);
@@ -209,8 +314,10 @@
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.Car);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "2D Subway Surfers meet Mario Kart";
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
             ((System.ComponentModel.ISupportInitialize)(this.Cone1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
@@ -224,8 +331,12 @@
             ((System.ComponentModel.ISupportInitialize)(this.Car)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.Cone2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.Cone3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Cone4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Cone6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Cone5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Cone7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Cone0)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -243,9 +354,17 @@
         private System.Windows.Forms.PictureBox Cone1;
         private System.Windows.Forms.PictureBox Cone2;
         private System.Windows.Forms.PictureBox Cone3;
-        private System.Windows.Forms.Timer timer2;
-        public System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Label Distance;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox Cone4;
+        private System.Windows.Forms.PictureBox Cone6;
+        private System.Windows.Forms.PictureBox Cone5;
+        private System.Windows.Forms.PictureBox Cone7;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.PictureBox Cone0;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
+using System.IO;
 
 namespace _2D_Subway_Surfers_meet_Mario_Kart
 {
@@ -17,52 +19,169 @@ namespace _2D_Subway_Surfers_meet_Mario_Kart
             InitializeComponent();
         }
 
-        int gamespead = 1;
-        int speedcache = 0;
+        
+        Random r = new Random(); //random
+        int _ticks; // varjabel för att hålla koll hur långt man kommit 
+        string Lose_cashe; //kasnke för att hålla 3 liv
+        
 
 
-
-
-
-
-
-        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        private void Form1_KeyDown(object sender, KeyEventArgs e)//antarar alla knappar
         {
-            if (e.KeyCode == Keys.A || e.KeyCode == Keys.Left)
+            if (e.KeyCode == Keys.A || e.KeyCode == Keys.Left) //för att styra
             {
-                if(Car.Left>50)
-                Car.Left += -55;
+                if (Car.Left > 55)
+                    Car.Left += -55;
             }
-            if (e.KeyCode == Keys.D || e.KeyCode == Keys.Right)
+            if (e.KeyCode == Keys.D || e.KeyCode == Keys.Right) //för att styra
             {
-                if (Car.Left < 200)
-                Car.Left += 55;
+                if (Car.Left < 255)
+                    Car.Left += 55;
+            }
+            if (e.KeyCode == Keys.W || e.KeyCode == Keys.Up)//för att köra upp bilen 
+            {
+                if (Car.Top > 100)
+                    Car.Top += -7;
+            }
+            if (e.KeyCode == Keys.S || e.KeyCode == Keys.Down)//för att få ner bilen 
+            {
+                if (Car.Top < 460)
+                    Car.Top += 7;
+            }
+            if (e.KeyCode == Keys.Enter) //enter för att starta om spel
+            {
+                Application.Restart();
+
             }
         }
 
         
-
-
-        void Movec()
+        void Random(Object Cone) //för att spåna hinder fd koner när de är utanför banan
         {
-            Cone1.Top += gamespead;
-            Cone2.Top++;
-            Cone3.Top++;
+            if (((PictureBox)Cone).Top > 580) // när de är utanför
+            {
+                int r1 = r.Next(1, 6);
+                r1 = r1 * 55;
+                ((PictureBox)Cone).Left = r1;
+                ((PictureBox)Cone).Top = -80;
+            }
         }
 
 
-
-
-        private void timer1_Tick(object sender, EventArgs e)
+        void MoveCone()//för att flutta fram alla 
         {
-            Distance.Text = ((gamespead*speedcache/10) + " m"); // skriver ut m
-            if (speedcache % 200 == 0)
+            Cone0.Top += 3;
+            Cone1.Top += 3;
+            Cone2.Top += 3;
+            Cone3.Top += 3;
+            Cone4.Top += 3;
+            Cone5.Top += 3;
+            Cone6.Top += 3;
+            Cone7.Top += 3;
+        }
+
+
+        void Gameover(Object Cone)//kör och kollar när bilen träffar konen
+        {
+            if (Car.Bounds.IntersectsWith(((PictureBox)Cone).Bounds)) //när bilen kroickar med conens .bound
             {
-                gamespead++;
+                Lose_cashe = Lose_cashe + "a"; //lägger till när man förlorar ett liv
+                panel1.Invalidate();   // updaterar livkara
+                ((PictureBox)Cone).Top = -80; //flyttar den högst upp
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)//Huvudlopen
+        {
+            _ticks++; // varjabel för att hålla koll hur långt man kommit 
+            Distance.Text = _ticks.ToString();
+            MoveCone();
+            Gameover(Cone0);
+            Gameover(Cone1);
+            Gameover(Cone2);
+            Gameover(Cone3);
+            Gameover(Cone4);
+            Gameover(Cone5);
+            Gameover(Cone6);
+            Gameover(Cone7);
+            if(Lose_cashe == "aaa")//när alla liv är slut
+            {
+                timer1.Enabled = false; //stänger av klockan
+                File.AppendAllText(@"c:\temp\score.txt", _ticks.ToString() + Environment.NewLine); //sparar alla spelgångar i en txt fil
+                panel2.Invalidate(); //för att uppdatera
+                panel2.Visible = true; //tar fram end screan
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e) //segare klocka för att flytta upp och spåna
+        {
+            Random(Cone0);
+            Random(Cone1);
+            Random(Cone2);
+            Random(Cone3);
+            Random(Cone4);
+            Random(Cone5);
+            Random(Cone6);
+            Random(Cone7);
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)//målar ut liven
+        {
+            Graphics g = e.Graphics;
+            SolidBrush b1 = new SolidBrush(Color.Orange);
+            SolidBrush b2 = new SolidBrush(Color.Black);
+
+            g.FillEllipse(b1, 7, 7, 26, 26);
+            g.FillEllipse(b1, 40, 7, 26, 26);
+            g.FillEllipse(b1, 73, 7, 26, 26);
+                        
+            if (Lose_cashe == "aaa")
+            {
+                g.FillEllipse(b2, 10, 10, 20, 20);
+            }
+            if (Lose_cashe == "aaa" || Lose_cashe == "aa")
+            {
+                g.FillEllipse(b2, 43, 10, 20, 20);
+            }
+            if (Lose_cashe == "aaa" || Lose_cashe == "aa" || Lose_cashe == "a")
+            {
+                g.FillEllipse(b2, 76, 10, 20, 20);
+            }          
+        }
+        
+        private void panel2_Paint(object sender, PaintEventArgs e)//endscrean
+        {
+            Graphics g = e.Graphics;
+            Font areal16 = new Font("Arial", 16);
+            Font areal14 = new Font("Arial", 14);
+            SolidBrush Fcolor = new SolidBrush(Color.Black);
+
+            g.DrawString("GAME OVER", areal16, Fcolor, 75, 15);
+            g.DrawString("Your score is " + _ticks + " m", areal16, Fcolor, 40, 40);
+            
+            string[] lines = File.ReadAllLines(@"c:\temp\score.txt");
+            int[] record = Array.ConvertAll(lines, s => int.Parse(s));
+            Array.Sort(record);
+            Array.Reverse(record);
+            
+            g.DrawString("Leaderboard:", areal16, Fcolor, 80, 70);
+            g.DrawString("1: " + (record[0]) + " m", areal16, Fcolor, 80, 90);
+            g.DrawString("2: " + (record[1]) + " m", areal16, Fcolor, 80, 110);
+            g.DrawString("3: " + (record[2]) + " m", areal16, Fcolor, 80, 130);
+            g.DrawString("4: " + (record[3]) + " m", areal16, Fcolor, 80, 150);
+            g.DrawString("5: " + (record[4]) + " m", areal16, Fcolor, 80, 170);
+
+            if (record[0] > _ticks)
+            {
+                int Need = record[0] - _ticks;
+                g.DrawString(Need + " m to brake record!", areal14, Fcolor, 25, 200);
+            }
+            else
+            {
+                g.DrawString("New Record!", areal16, Fcolor, 70, 200);
 
             }
-            speedcache++;
-            Movec();
+            g.DrawString("Press Enter to Restart", areal16, Fcolor, 25, 250);
         }
     }
 }
